930849f9a346af6b4cf131231e1f15f8b1e02fa0
Jaime Manuel Caballero Tello <jcaballe@login03.pitagora.local>
2025-09-01 17:19:47 +0200

    Add configuration and build scripts for Pitagora GPU

---
+++ machines/mkdeps.pitagora-gpu.sh
@@ -0,0 +1,10 @@
+# available versions of cuda bla bla in pitagora need to check if these work
+
+module load cuda/12.6
+module load module load openmpi/4.1.6--gcc--12.3.0
+module load nccl/2.22.3-1--gcc--12.3.0
+module load cmake/3.27.9
+
+cd install-deps
+: "${PREFIX:=$HOME/gkylsoft}"
+./mkdeps.sh --build-openblas=yes --build-superlu=yes --build-luajit=yes --build-cudss=yes --build-adas=yes --prefix=$PREFIX
